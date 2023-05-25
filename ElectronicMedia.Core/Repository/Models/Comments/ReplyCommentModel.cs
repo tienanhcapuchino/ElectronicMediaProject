a7a25d4ddf59d1f1bbb6d3eeb24f33037117959c
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-05-25 23:42:29 +0700

    [Back-end] reply comment service, CRUD

--- ElectronicMedia.Core/Repository/Models/Comments/ReplyCommentModel.cs
+++ ElectronicMedia.Core/Repository/Models/Comments/ReplyCommentModel.cs
@@ -11,8 +11,6 @@ namespace ElectronicMedia.Core.Repository.Models
     {
         public Guid ParentId { get; set; }
         public string Content { get; set; }
-        public User User { get; set; }
         public Guid UserId { get; set; }
-        public DateTime CreatedDate { get; set; }
     }
 }
