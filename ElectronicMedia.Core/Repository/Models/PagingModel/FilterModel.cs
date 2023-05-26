8a0df0285b7d2bf0ff4cd12c359aba94be564e4f
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-05-26 21:11:10 +0700

    [Back-end] Paging model

---
+++ ElectronicMedia.Core/Repository/Models/PagingModel/FilterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicMedia.Core.Repository.Models
+{
+    public class FilterModel
+    {
+        public int PageIndex { get; set; }
+        public string? FilterValue { get; set; }
+        public int PageSize { get; set; }
+    }
+}
