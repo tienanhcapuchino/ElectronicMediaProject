86b1a2a8a20270bd01521f343d627e46563b1233
VietLB01 <114459411+VietLB01@users.noreply.github.com>
2023-06-16 00:47:25 +0700

    add authen

---
+++ ElectronicMedia.Core/Common/UserRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicMedia.Core.Common
+{
+    public static class UserRole
+    {
+
+        public const string Admin = "Admin";
+        public const string NormalUser = "NormalUser";
+        public const string Writer = "Writer";
+        public const string Leader = "Leader";
+        public const string EditorDirector = "EditorDirector";
+        public static readonly List<string> Roles = new List<string>() { Admin, NormalUser, Writer, Leader, EditorDirector };
+    }
+}
